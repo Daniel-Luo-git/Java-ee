9c27dd3a46006390493e1cb210e8456ec4e1defa
Daniel-Luo-git <luoshiweiyx@outlook.com>
2018-03-17 11:40:04 +0800

    destory方法更改

--- java/HelloSpring/src/com/tutorialspoint/HelloWorld.java
+++ java/HelloSpring/src/com/tutorialspoint/HelloWorld.java
@@ -12,7 +12,7 @@ public class HelloWorld {
 		      System.out.println("Bean is going through init.");
 		   }
 	   public void destroy(){
-		      System.out.println("Bean will destroy now.");
+		      System.out.println("Bean will destroy.");
 		   }
 		  
 	}
